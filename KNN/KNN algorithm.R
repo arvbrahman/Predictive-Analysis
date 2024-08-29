5eae9bc407edc4b6458f210d5c876858393fc32a
abhishek <arvbrahman1037@gmail.com>
2024-08-29 22:29:40 +0530

    Knn added

--- KNN/KNN algorithm.R
+++ KNN/KNN algorithm.R
@@ -1,23 +1,23 @@
-data <- read.csv(file = "wisc_bc_data",stringsAsFactors = F)   #the original data
+data <- read.csv(file = "wbcd.csv",stringsAsFactors = F)   #Loading the original data
 
-wdbc <- data[,-1]     #removing the first column and saving it 
+wbcd <- data[,-1]     #removing the first column and saving it 
 
 data_norm <- function(x){    #Used to normalize the data between 0 and 1
   ((x-min(x))/(max(x)-min(x)))       
 }
 
-wdbc_norm <- as.data.frame(lapply(wdbc[,-1], data_norm)) 
-summary(wdbc_norm)
+wbcd_norm <- as.data.frame(lapply(wbcd[,-1], data_norm)) 
+str(wbcd_norm)
 
-#separating data for training and test
-wdbc_train <- wdbc_norm[1:450,]
-wdbc_test <- wdbc_norm[451:569,]
+#separating data for training and testing
+wbcd_train <- wbcd_norm[1:450,]
+wbcd_test <- wbcd_norm[451:569,]
 
 library(class)       #the package that contains Knn function
-wdbc_pred <- knn(wdbc_train,wdbc_test,wdbc[1:450,1],k=24)
-table(wdbc_pred,wdbc[451:569,1])
+wbcd_pred <- knn(wbcd_train,wbcd_test,wbcd[1:450,1],k=24)
+table(wbcd_pred,wbcd[451:569,1])
 
-
+#To check the results on a crosstable
 install.packages("gmodels")
 library(gmodels)
-CrossTable(wdbc[451:569,1],wdbc_pred,prop.chisq = F)
+CrossTable(wbcd[451:569,1],wbcd_pred,prop.chisq = F)
