421642e0c29b114b7096dc0900fd5b79184e4ad1
abhishek <arvbrahman1037@gmail.com>
2024-09-13 00:12:44 +0530

    prostate cancer data set

--- KNN/knn on prostate cancer.R
+++ KNN/knn on prostate cancer.R
@@ -2,18 +2,22 @@
 prostate_data <- read.csv(file.choose(),stringsAsFactors = F)
 prostate_data <- prostate_data[,-1]
 
+#Function for normalization
 norm <- function(x){
   (x-min(x))/(max(x)-min(x))
 }
 
-prostate_norm <- as.data.frame(lapply(prostate_data[,-1],norm))
+prostate_norm <- as.data.frame(lapply(prostate_data[,-1],norm))  #standardizing
 
+#Splitting the Data into train and test
 prostate_train <- prostate_norm[1:70,]
 prostate_test <- prostate_norm[71:100,]
 
-library(class)
+#KNN
+library(class)  #class library conatins knn
 prostate_pred <- knn(prostate_train,prostate_test,prostate_data[1:70,1],k=6)
 table(prostate_pred,prostate_data[71:100,1])
 
-library(gmodels)
+#Evaluation
+library(gmodels) #gmodels conatins crosstable
 CrossTable(prostate_data[71:100,1],prostate_pred,prop.chisq = F)
