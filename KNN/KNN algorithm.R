09a80d502a04177756430e4451676f5eb0fdd1e6
abhishek <arvbrahman1037@gmail.com>
2024-09-15 23:18:25 +0530

    knn

--- KNN/KNN algorithm.R
+++ KNN/KNN algorithm.R
@@ -9,6 +9,7 @@ data_norm <- function(x){    #Used to normalize the data between 0 and 1
 wbcd_norm <- as.data.frame(lapply(wbcd[,-1], data_norm)) 
 str(wbcd_norm)
 
+
 #separating data for training and testing
 wbcd_train <- wbcd_norm[1:450,]
 wbcd_test <- wbcd_norm[451:569,]
@@ -16,6 +17,7 @@ wbcd_test <- wbcd_norm[451:569,]
 library(class)       #the package that contains Knn function
 wbcd_pred <- knn(wbcd_train,wbcd_test,wbcd[1:450,1],k=24)
 table(wbcd_pred,wbcd[451:569,1])
+
 
 #To check the results on a Crosstable
 install.packages("gmodels")
