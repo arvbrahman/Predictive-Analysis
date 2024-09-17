fc52467a34e5cae172de685c8810e63f3d2e33a1
abhishek <arvbrahman1037@gmail.com>
2024-09-18 00:01:27 +0530

    naive bayes too low accuracy

--- Naive Bayes/Naive Bayes.R
+++ Naive Bayes/Naive Bayes.R
@@ -1,37 +1,56 @@
-#Loading necessary libraries
+#Loading necessary libraries ----
+library(dplyr)
 library(tm)   #tm: text mining library is used for text cleaning 
+library(SnowballC)
 library(caret)
 library(e1071)
-library(dplyr)
+library(gmodels)
 
-#Loading the data
+#Loading the data ----
 mail_Data <- read.csv(file = "spam.csv",stringsAsFactors = F)
 
-#Observing the data 
+#Observing the data ----
 str(mail_Data)
 table(mail_Data$type)
 
+#cleaning and transforming the data ----
 #Removing unnecessary columns &
 #converting label into categorical variable which makes it easy for classification
 mail_Data <- mail_Data[,c("type","text")]
 mail_Data$type <- as.factor(mail_Data$type)
-str(mail_Data)
 
+#Data Preprocessing ----
+#text cleaning in this data set 
+mail_corpus <- iconv(mail_Data$text, to = "UTF-8", sub = "")
 
-#Data Cleaning Or text cleaning in this data set 
-mail_corpus_clean <- iconv(mail_Data$text)%>%
-  Corpus(VectorSource)%>%
+mail_corpus_clean<-  Corpus(VectorSource(mail_corpus))%>%
   tm_map(content_transformer(tolower))%>%
   tm_map(removeNumbers)%>%
   tm_map(removePunctuation)%>%
-  tm_map(removeWords,stopwords("en"))
+  tm_map(removeWords,stopwords("en"))%>%
+  tm_map(stripWhitespace)%>%
+  tm_map(stemDocument)
 
+#DTM
+mail_dtm <- DocumentTermMatrix(mail_corpus_clean)
 
+#Splitting the data ----
+set.seed(123)
+trainIndex <- createDataPartition(mail_Data$type, p = 0.7, list = F)
+mail_train_data <- mail_dtm[trainIndex, ]
+mail_test_data <- mail_dtm[-trainIndex, ]
+mail_train_labels <- mail_Data$type[trainIndex]
+mail_test_labels <- mail_Data$type[-trainIndex]
 
-#Stemming
-library(SnowballC)
-mail_clean <- tm_map(mail_clean,stemDocument)
-inspect(mail_clean[1:2])
-mail_dtm <- DocumentTermMatrix(mail_clean)
+common_features <- intersect(colnames(mail_train_data), colnames(mail_test_data))
+mail_train_data <- mail_train_data[, common_features]
+mail_test_data <- mail_test_data[, common_features]
 
+#Training the model ----
+model <- naiveBayes(as.matrix(mail_train_data),mail_train_labels)
+predictions <- predict(model,as.matrix(mail_test_data))
 
+#Evaluating ----
+confusionMatrix(predictions,mail_test_labels)
+
+CrossTable(predictions,mail_test_labels,prop.chisq = F)
