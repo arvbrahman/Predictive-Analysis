ecd1251bc8b6a8be16332af0bdaa9c2997a392bf
abhishek <arvbrahman1037@gmail.com>
2024-09-18 16:46:41 +0530

    Naive Bayes

--- Naive Bayes/Naive Bayes.R
+++ Naive Bayes/Naive Bayes.R
@@ -1,10 +1,11 @@
 #Loading necessary libraries ----
-library(dplyr)
+library(dplyr)   #for transforming data and contains pipe operator
 library(tm)   #tm: text mining library is used for text cleaning 
-library(SnowballC)
-library(caret)
-library(e1071)
-library(gmodels)
+library(SnowballC)   #Library for stemming
+library(caret)    #Library for splitting data
+library(wordcloud)   #library for pictorial representation of frequency
+library(e1071)   #package for NaiveBayes
+library(gmodels)  #library for crosstable for confusionmatrix
 
 #Loading the data ----
 mail_Data <- read.csv(file = "spam.csv",stringsAsFactors = F)
@@ -21,13 +22,13 @@ mail_Data$type <- as.factor(mail_Data$type)
 
 #Data Preprocessing ----
 #text cleaning in this data set 
-mail_corpus <- iconv(mail_Data$text, to = "UTF-8", sub = "")
+mail_corpus <- iconv(mail_Data$text)
 
 mail_corpus_clean<-  Corpus(VectorSource(mail_corpus))%>%
   tm_map(content_transformer(tolower))%>%
   tm_map(removeNumbers)%>%
   tm_map(removePunctuation)%>%
-  tm_map(removeWords,stopwords("en"))%>%
+  tm_map(removeWords,stopwords())%>%
   tm_map(stripWhitespace)%>%
   tm_map(stemDocument)
 
@@ -36,18 +37,26 @@ mail_dtm <- DocumentTermMatrix(mail_corpus_clean)
 
 #Splitting the data ----
 set.seed(123)
-trainIndex <- createDataPartition(mail_Data$type, p = 0.7, list = F)
+trainIndex <- createDataPartition(mail_Data$type, p = 0.8, list = F)
 mail_train_data <- mail_dtm[trainIndex, ]
 mail_test_data <- mail_dtm[-trainIndex, ]
 mail_train_labels <- mail_Data$type[trainIndex]
 mail_test_labels <- mail_Data$type[-trainIndex]
 
-common_features <- intersect(colnames(mail_train_data), colnames(mail_test_data))
-mail_train_data <- mail_train_data[, common_features]
-mail_test_data <- mail_test_data[, common_features]
+
+#Wordcloud ----
+wordcloud(mail_corpus_clean,min.freq = 50,
+          random.order = F,colors = brewer.pal(5,"Dark2"))
+
+#Frequency
+findFreqTerms(mail_train_data,5)
+mail_freq_words <- findFreqTerms(mail_train_data, 5)
+str(mail_freq_words)
+mail_freq_train<- mail_train_data[ , mail_freq_words]
+mail_freq_test <- mail_test_data[ , mail_freq_words]
 
 #Training the model ----
-model <- naiveBayes(as.matrix(mail_train_data),mail_train_labels)
+model <- naiveBayes(as.matrix(mail_freq_train),mail_train_labels)
 predictions <- predict(model,as.matrix(mail_test_data))
 
 #Evaluating ----
---
+++ Naive Bayes/Naive.R
@@ -0,0 +1,67 @@
+#Loading necessary libraries ----
+library(dplyr)   #for transforming data and contains pipe operator
+library(tm)   #tm: text mining library is used for text cleaning 
+library(SnowballC)   #Library for stemming
+library(caret)    #Library for splitting data
+library(wordcloud)   #library for pictorial representation of frequency
+library(e1071)   #package for NaiveBayes
+library(gmodels)  #library for crosstable for confusionmatrix
+
+
+#loading data ----
+sms_raw <- read.csv(file = "spam.csv", stringsAsFactors = FALSE) 
+
+
+#Cleaning  ----
+sms_raw$type <- factor(sms_raw$type)
+sms_raw <- sms_raw[,c("type","text")]
+
+
+#Text Cleaning: ----
+sms_corpus<-iconv(sms_raw$text)
+sms_corpus_clean <- Corpus(VectorSource(sms_corpus))%>%
+  tm_map(content_transformer(tolower))%>%
+  tm_map(removeNumbers)%>%
+  tm_map(removeWords, stopwords())%>%
+  tm_map(removePunctuation)%>%
+  tm_map(stemDocument)%>%
+  tm_map(stripWhitespace)
+
+
+#Document-Term Matrix (DTM) & splitting ----
+sms_dtm <- DocumentTermMatrix(sms_corpus_clean)
+sms_dtm_train <- sms_dtm[1:4169, ]
+sms_dtm_test <- sms_dtm[4170:5559, ]
+sms_train_labels <- sms_raw[1:4169, ]$type 
+sms_test_labels <- sms_raw[4170:5559, ]$type
+
+
+#Word Cloud ----
+wordcloud(sms_corpus_clean, min.freq = 50, 
+          random.order = FALSE,colors=brewer.pal(5,"Dark2")) 
+
+#findFreqTerms
+sms_freq_words <- findFreqTerms(sms_dtm_train, 5)
+str(sms_freq_words)
+sms_dtm_freq_train<- sms_dtm_train[ , sms_freq_words]
+sms_dtm_freq_test <- sms_dtm_test[ , sms_freq_words]
+
+#convert_counts ----
+convert_counts <- function(x) {
+  x <- ifelse(x > 0, "Yes", "No")
+}
+sms_train <- apply(sms_dtm_freq_train, MARGIN = 2,
+                   convert_counts)
+sms_test <- apply(sms_dtm_freq_test, MARGIN = 2,
+                  convert_counts)
+
+
+#Model training ----
+sms_classifier <- naiveBayes(sms_train, sms_train_labels)
+sms_test_pred <- predict(sms_classifier, sms_test)
+
+
+#CrossTable ----
+CrossTable(sms_test_pred, sms_test_labels,
+           prop.chisq = FALSE, prop.t = FALSE,
+           dnn = c('predicted', 'actual')) 
